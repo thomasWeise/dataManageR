0f43d467049a904af31ab277e5bcd3be5a2b99c3
Thomas Weise <tweise@hfuu.edu.cn>
2018-07-18 06:27:40 +0800

    Documentation Fix

--- R/featuresDefaultLoader.R
+++ R/featuresDefaultLoader.R
@@ -5,6 +5,7 @@
 #'   \code{\link{features.txtLoader}}
 #' @param featureFolder the folder with the features
 #' @param components the components
+#' @param values.eval the evaluated values
 #' @return a named list, where the names are the feature names and the values
 #'   the corresponding feature values
 #' @export features.defaultLoader
