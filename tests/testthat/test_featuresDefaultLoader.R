013d3d591d1539b9bf9a612cc5f1a65db7a7ad45
Thomas Weise <tweise@hfuu.edu.cn>
2018-07-18 11:55:19 +0800

    Fixed Typo in Test

--- tests/testthat/test_featuresDefaultLoader.R
+++ tests/testthat/test_featuresDefaultLoader.R
@@ -2,5 +2,5 @@ library("dataManageR")
 context("datasets.feature.load.default")
 
 test_that("Test datasets.feature.load.text", {
-  expect_identica(datasets.feature.load.default, datasets.feature.load.text);
+  expect_identical(datasets.feature.load.default, datasets.feature.load.text);
 })
